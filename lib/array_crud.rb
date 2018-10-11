fa6635f4d85c38990569ea433a074c7f1fb31c8c
Leslie Osorio <lesosorio17@gmail.com>
2018-10-11 18:25:07 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-family_members << "mother bear"
+family_members <<"mother bear"
 end
 
 def add_element_to_start_of_array(array, element)
