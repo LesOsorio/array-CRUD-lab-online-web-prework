5aad68f165e450b9c860eb045e3da3892a0aaef6
Leslie Osorio <lesosorio17@gmail.com>
2018-10-11 18:26:00 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  family_members = ["les","jon","toby","charlie"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+family_members <<"mother bear"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+family_members.unshift("father bear")
 end
 
 def remove_element_from_end_of_array(array)
-  
+family_members.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+family_members.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+family_members[0]
 end
 
 def retrieve_first_element_from_array(array)
-  
+family_members[1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+family_members[2]
 end
