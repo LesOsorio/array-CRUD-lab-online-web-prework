b9137ecf18e5ba984dfd9791abda3d85e5b2b7ae
Leslie Osorio <lesosorio17@gmail.com>
2018-10-11 18:23:20 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ family_members.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+family_members.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+family_members[0]
 end
 
 def retrieve_first_element_from_array(array)
